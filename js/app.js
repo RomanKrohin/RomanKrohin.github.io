fc7c1fa2e9a6e08fe7b415cd4dc70c825e8a451f
Roman <romankrohin126@gmail.com>
2024-04-13 00:49:27 +0300

    Fix 16

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 let tg = window.Telegram.WebApp;
 
-let all_cost =[0,0,0,0,0,0]
-
+let all_cost = [0, 0, 0, 0, 0, 0]
 
 tg.expand();
 
@@ -12,64 +11,53 @@ let item = "";
 
 let btn1 = document.getElementById("btn1");
 
-function addPlusMinusButtons(item) {
-    // Создаем кнопку "+" и добавляем обработчик события
-    let plusButton = document.createElement("button");
-    plusButton.innerText = "+";
-    plusButton.classList.add("btn");
-    plusButton.classList.add("plus");
-    plusButton.addEventListener("click", function() {
-        // Увеличиваем количество товара
-        let quantityElement = document.getElementById(`quantity${item}`);
-        let currentQuantity = parseInt(quantityElement.innerText);
-        quantityElement.innerText = currentQuantity + 1;
-    });
+btn1.addEventListener("click", function () {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    } else {
+        all_cost[0] = all_cost[0] + 1;
+        tg.MainButton.setText(all_cost[0] * 10);
+        item = "1";
 
-    // Создаем кнопку "-" и добавляем обработчик события
-    let minusButton = document.createElement("button");
-    minusButton.innerText = "-";
-    minusButton.classList.add("btn");
-    minusButton.classList.add("minus");
-    minusButton.addEventListener("click", function() {
-        // Уменьшаем количество товара, но не ниже 0
-        let quantityElement = document.getElementById(`quantity${item}`);
-        let currentQuantity = parseInt(quantityElement.innerText);
-        if (currentQuantity > 0) {
-            quantityElement.innerText = currentQuantity - 1;
-        }
-    });
+        // Создание кнопки "+"
+        let plusButton = document.createElement("button");
+        plusButton.innerText = "+";
+        plusButton.classList.add("btn");
+        plusButton.classList.add("plus");
+        plusButton.addEventListener("click", function () {
+            all_cost[0]++;
+            tg.MainButton.setText(all_cost[0] * 10);
+        });
 
-    // Добавляем кнопки "+" и "-" после кнопки "Add"
-    let addButton = document.getElementById(`btn${item}`);
-    addButton.insertAdjacentElement('afterend', plusButton);
-    addButton.insertAdjacentElement('afterend', minusButton);
-}
+        // Создание кнопки "-"
+        let minusButton = document.createElement("button");
+        minusButton.innerText = "-";
+        minusButton.classList.add("btn");
+        minusButton.classList.add("minus");
+        minusButton.addEventListener("click", function () {
+            if (all_cost[0] > 0) {
+                all_cost[0]--;
+                tg.MainButton.setText(all_cost[0] * 10);
+            }
+        });
 
-btn1.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		all_cost[0]=all_cost[0]+1
-		tg.MainButton.setText(all_cost[0]*10);
-		item = "1";
-		addPlusMinusButtons(1);
-		tg.MainButton.show();
-	}
+        // Добавление кнопок "+" и "-" к элементу DOM
+        let addButton = document.getElementById(`btn${item}`);
+        addButton.insertAdjacentElement('afterend', plusButton);
+        addButton.insertAdjacentElement('afterend', minusButton);
+
+        tg.MainButton.show();
+    }
 });
 
-
-
-Telegram.WebApp.onEvent("mainButtonClicked", function(){
-	tg.sendData(item);
+Telegram.WebApp.onEvent("mainButtonClicked", function () {
+    tg.sendData(item);
 });
-
 
 let usercard = document.getElementById("usercard");
 
 let p = document.createElement("p");
 
-p.innerText = `${tg.initDataUnsafe.user.first_name}
-${tg.initDataUnsafe.user.last_name}`;
+p.innerText = `${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
 
 usercard.appendChild(p);
