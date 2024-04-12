3495eb62c02a78f2355c4bc7c73b03c1daa99699
Roman <romankrohin126@gmail.com>
2024-04-13 01:19:03 +0300

    Fix 32

--- js/app.js
+++ js/app.js
@@ -1,67 +1,67 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let tg = window.Telegram.WebApp;
+let tg = window.Telegram.WebApp;
 
-    let all_cost = [0, 0, 0, 0, 0, 0];
+let all_cost = [0, 0, 0, 0, 0, 0]
 
-    tg.expand();
+tg.expand();
 
-    tg.MainButton.textColor = '#FFFFFF';
-    tg.MainButton.color = '#2cab37';
+tg.MainButton.textColor = '#FFFFFF';
+tg.MainButton.color = '#2cab37';
 
-    let item = "";
+let item = "";
 
-    let btn1 = document.getElementById("btn1");
+let btn1 = document.getElementById("btn1");
 
-    btn1.addEventListener("click", function () {
-        if (tg.MainButton.isVisible) {
-            tg.MainButton.hide();
-        } else {
-            all_cost[0] = all_cost[0] + 1;
+btn1.addEventListener("click", function () {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    } else {
+        all_cost[0] = all_cost[0] + 1;
+        tg.MainButton.setText(all_cost[0] * 10);
+        item = "1";
+
+        // Создание кнопки "+"
+        let plusButton = document.createElement("button");
+        plusButton.innerText = "+";
+        plusButton.classList.add("btn");
+        plusButton.classList.add("plus");
+        plusButton.addEventListener("click", function () {
+            all_cost[0]++;
+			document.getElementById("quantity1").innerText = all_cost[0]
+			document.getElementById("quantity1").innerHTML = all_cost[0]
+			document.getElementById("quantity1").insertAdjacentText(all_cost[0])
             tg.MainButton.setText(all_cost[0] * 10);
-            item = "1";
+        });
 
-            // Создание кнопки "+"
-            let plusButton = document.createElement("button");
-            plusButton.innerText = "+";
-            plusButton.classList.add("btn");
-            plusButton.classList.add("plus");
-            plusButton.addEventListener("click", function () {
-                all_cost[0]++;
-                document.getElementById("quantity1").innerText = all_cost[0]; // Обновляем значение
+        // Создание кнопки "-"
+        let minusButton = document.createElement("button");
+        minusButton.innerText = "-";
+        minusButton.classList.add("btn");
+        minusButton.classList.add("minus");
+        minusButton.addEventListener("click", function () {
+            if (all_cost[0] > 0) {
+                all_cost[0]--;
+				document.getElementById("quantity1").innerText = all_cost[0]
                 tg.MainButton.setText(all_cost[0] * 10);
-            });
+            }
+        });
 
-            // Создание кнопки "-"
-            let minusButton = document.createElement("button");
-            minusButton.innerText = "-";
-            minusButton.classList.add("btn");
-            minusButton.classList.add("minus");
-            minusButton.addEventListener("click", function () {
-                if (all_cost[0] > 0) {
-                    all_cost[0]--;
-                    document.getElementById("quantity1").innerText = all_cost[0]; // Обновляем значение
-                    tg.MainButton.setText(all_cost[0] * 10);
-                }
-            });
+        // Добавление кнопок "+" и "-" к элементу DOM
+        let addButton = document.getElementById(`btn${item}`);
+        addButton.insertAdjacentElement('afterend', plusButton);
+        addButton.insertAdjacentElement('afterend', minusButton);
 
-            // Добавление кнопок "+" и "-" к элементу DOM
-            let addButton = document.getElementById(`btn${item}`);
-            addButton.insertAdjacentElement('afterend', plusButton);
-            addButton.insertAdjacentElement('afterend', minusButton);
-
-            tg.MainButton.show();
-        }
-    });
-
-    Telegram.WebApp.onEvent("mainButtonClicked", function () {
-        tg.sendData(JSON.stringify(all_cost));
-    });
-
-    let usercard = document.getElementById("usercard");
-
-    let p = document.createElement("p");
-
-    p.innerText = `${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
-
-    usercard.appendChild(p);
+        tg.MainButton.show();
+    }
 });
+
+Telegram.WebApp.onEvent("mainButtonClicked", function () {
+    tg.sendData(JSON.stringify(all_cost));
+});
+
+let usercard = document.getElementById("usercard");
+
+let p = document.createElement("p");
+
+p.innerText = `${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
+
+usercard.appendChild(p);
