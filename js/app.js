3fa35e8aeaea8360ea8f1f288e27ac2aa17b3d08
Roman <romankrohin126@gmail.com>
2024-04-13 01:57:52 +0300

    Fix 43

--- js/app.js
+++ js/app.js
@@ -16,9 +16,9 @@ plusButton.innerText = "+";
 plusButton.classList.add("btn");
 plusButton.classList.add("plus");
 plusButton.addEventListener("click", function () {
-    all_cost[0]++;
-    tg.MainButton.setText(all_cost[0] * 10);
-    updateQuantityDisplay();
+	all_cost[0]++;
+	tg.MainButton.setText(all_cost[0] * 10);
+	updateQuantityDisplay();
 });
 
 let minusButton = document.createElement("button");
@@ -26,42 +26,42 @@ minusButton.innerText = "-";
 minusButton.classList.add("btn");
 minusButton.classList.add("minus");
 minusButton.addEventListener("click", function () {
-    if (all_cost[0] > 0) {
-        all_cost[0]--;
-        tg.MainButton.setText(all_cost[0] * 10);
-        updateQuantityDisplay();
-    }
+	if (all_cost[0] > 0) {
+		all_cost[0]--;
+		tg.MainButton.setText(all_cost[0] * 10);
+		updateQuantityDisplay();
+	}
 });
 
 let usercard = document.getElementById("usercard");
 let q1 = document.getElementById("quantity1");
 
 function updateQuantityDisplay() {
-    q1.innerText = all_cost[0];
+	q1.innerText = all_cost[0];
 }
 
 btn1.addEventListener("click", function () {
-    if (tg.MainButton.isVisible) {
-        tg.MainButton.hide();
-    } else {
-        all_cost[0]++;
-        tg.MainButton.setText(all_cost[0] * 10);
-        updateQuantityDisplay();
-        showPlusMinusButtons();
-        tg.MainButton.show();
-    }
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	} else {
+		all_cost[0]++;
+		tg.MainButton.setText(all_cost[0] * 10);
+		updateQuantityDisplay();
+		showPlusMinusButtons();
+		tg.MainButton.show();
+	}
 });
 
 function showPlusMinusButtons() {
-    btn1.style.display = "none";
-    plusButton.style.display = "inline-block";
-    minusButton.style.display = "inline-block";
+	btn1.style.display = "none";
+	plusButton.style.display = "inline-block";
+	minusButton.style.display = "inline-block";
 }
 
 function showAddButton() {
-    btn1.style.display = "inline-block";
-    plusButton.style.display = "none";
-    minusButton.style.display = "none";
+	btn1.style.display = "inline-block";
+	plusButton.style.display = "none";
+	minusButton.style.display = "none";
 }
 
 let p = document.createElement("p");
@@ -72,5 +72,5 @@ updateQuantityDisplay();
 showAddButton();
 
 Telegram.WebApp.onEvent("mainButtonClicked", function () {
-    tg.sendData(JSON.stringify(all_cost));
+	tg.sendData(JSON.stringify(all_cost));
 });
