5e0f0fe5339b58f9f1166bbc32e5add86ea9c7da
Roman <romankrohin126@gmail.com>
2024-04-13 02:59:58 +0300

    1234234678iyjtrg

--- js/app.js
+++ js/app.js
@@ -12,44 +12,7 @@ let item = "";
 let btn1 = document.getElementById("btn1");
 
 btn1.addEventListener("click", function () {
-    if (tg.MainButton.isVisible) {
-        tg.MainButton.hide();
-    } else {
-        all_cost[0] = all_cost[0] + 1;
-        updateQuantity(0, all_cost[0]); // Обновляем количество товара
-        tg.MainButton.setText(all_cost[0] * 123);
-        item = "1";
-        // Создание кнопки "+"
-        let plusButton = document.createElement("button");
-        plusButton.innerText = "+";
-        plusButton.classList.add("btn");
-        plusButton.classList.add("plus");
-        plusButton.addEventListener("click", function () {
-            all_cost[0]++;
-            updateQuantity(0, all_cost[0]); // Обновляем количество товара
-            tg.MainButton.setText(all_cost[0] * 123);
-        });
 
-        // Создание кнопки "-"
-        let minusButton = document.createElement("button");
-        minusButton.innerText = "-";
-        minusButton.classList.add("btn");
-        minusButton.classList.add("minus");
-        minusButton.addEventListener("click", function () {
-            if (all_cost[0] > 0) {
-                all_cost[0]--;
-                updateQuantity(0, all_cost[0]); // Обновляем количество товара
-                tg.MainButton.setText(all_cost[0] * 123);
-            }
-        });
-
-        // Добавление кнопок "+" и "-" к элементу DOM
-        let addButton = document.getElementById(`btn${item}`);
-        addButton.insertAdjacentElement('afterend', plusButton);
-        addButton.insertAdjacentElement('afterend', minusButton);
-
-        tg.MainButton.show();
-    }
 });
 
 Telegram.WebApp.onEvent("mainButtonClicked", function () {
