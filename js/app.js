e6d231be81ff68d5aad441d75d83340bdfcf99fb
Roman <romankrohin126@gmail.com>
2024-04-13 01:09:41 +0300

    Fix 28

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 let tg = window.Telegram.WebApp;
 
-let all_cost = [0, 0, 0, 0, 0, 0];
+let all_cost = [0, 0, 0, 0, 0, 0]
 
 tg.expand();
 
@@ -15,19 +15,21 @@ btn1.addEventListener("click", function () {
     if (tg.MainButton.isVisible) {
         tg.MainButton.hide();
     } else {
-        all_cost[0]++;
-        updateMainButtonAndCost(); // Вызываем функцию обновления после изменения количества товаров
+        all_cost[0] = all_cost[0] + 1;
+        tg.MainButton.setText(all_cost[0] * 10);
         item = "1";
 
+        // Создание кнопки "+"
         let plusButton = document.createElement("button");
         plusButton.innerText = "+";
         plusButton.classList.add("btn");
         plusButton.classList.add("plus");
         plusButton.addEventListener("click", function () {
             all_cost[0]++;
-            updateMainButtonAndCost(); // Вызываем функцию обновления после изменения количества товаров
+            tg.MainButton.setText(all_cost[0] * 10);
         });
 
+        // Создание кнопки "-"
         let minusButton = document.createElement("button");
         minusButton.innerText = "-";
         minusButton.classList.add("btn");
@@ -35,10 +37,11 @@ btn1.addEventListener("click", function () {
         minusButton.addEventListener("click", function () {
             if (all_cost[0] > 0) {
                 all_cost[0]--;
-                updateMainButtonAndCost(); // Вызываем функцию обновления после изменения количества товаров
+                tg.MainButton.setText(all_cost[0] * 10);
             }
         });
 
+        // Добавление кнопок "+" и "-" к элементу DOM
         let addButton = document.getElementById(`btn${item}`);
         addButton.insertAdjacentElement('afterend', plusButton);
         addButton.insertAdjacentElement('afterend', minusButton);
@@ -48,17 +51,13 @@ btn1.addEventListener("click", function () {
 });
 
 Telegram.WebApp.onEvent("mainButtonClicked", function () {
-    tg.sendData(JSON.stringify(all_cost));
+    tg.sendData(item);
 });
 
 let usercard = document.getElementById("usercard");
 
 let p = document.createElement("p");
-p.innerText = `${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
-usercard.appendChild(p);
 
-// Функция для обновления текста главной кнопки и количества товаров
-function updateMainButtonAndCost() {
-    tg.MainButton.setText(all_cost.reduce((total, current) => total + current, 0) * 10); // Обновляем текст главной кнопки
-    document.getElementById('quantity1').innerText = all_cost[0]; // Обновляем количество товара
-}
+p.innerText = `${tg.initDataUnsafe.user.first_name} ${tg.initDataUnsafe.user.last_name}`;
+
+usercard.appendChild(p);
