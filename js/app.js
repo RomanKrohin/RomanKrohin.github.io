009b7ed6fd33460dcc054084b45a8a4f4ffd47c8
Roman <romankrohin126@gmail.com>
2024-04-13 03:38:35 +0300

    Fix final

--- js/app.js
+++ js/app.js
@@ -7,14 +7,18 @@ tg.expand();
 tg.MainButton.textColor = '#FFFFFF';
 tg.MainButton.color = '#2cab37';
 let m1 = false;
+let m2 = false;
+let m3 = false;
 
 let item = "";
 
 let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
 
 function checkMainButton(){
-	if (all_cost[0]>0){
-		tg.MainButton.setText(all_cost[0] * 10)
+	if (all_cost[0]+all_cost[1]+all_cost[2]>0){
+		tg.MainButton.setText(all_cost[0] * 10+all_cost[1]*20+all_cost[2]*30)
 	}
 	else{
 		tg.MainButton.hide();
@@ -26,7 +30,7 @@ btn1.addEventListener("click", function () {
         all_cost[0] = all_cost[0] + 1;
 		checkMainButton();
         updateQuantity(0, all_cost[0]);
-        tg.MainButton.setText(all_cost[0] * 10);
+        tg.MainButton.setText(all_cost[0] * 10+all_cost[1]*20+all_cost[2]*30);
         item = "1";
         if (!m1 && all_cost[0]>0){
 			m1= true
@@ -39,7 +43,7 @@ btn1.addEventListener("click", function () {
 					all_cost[0]--;
 					checkMainButton();
 					updateQuantity(0, all_cost[0]);
-					tg.MainButton.setText(all_cost[0] * 10);
+					tg.MainButton.setText(all_cost[0] * 10+all_cost[1]*20+all_cost[2]*30);
 					if (all_cost[0]==0){
 						minusButton.remove();
 						m1=false
@@ -54,6 +58,74 @@ btn1.addEventListener("click", function () {
 			addButton.insertAdjacentElement('afterend', minusButton);
 		}
         tg.MainButton.show();
+});
+
+btn2.addEventListener("click", function () {
+	all_cost[1] = all_cost[1] + 1;
+	checkMainButton();
+	updateQuantity(1, all_cost[1]);
+	tg.MainButton.setText(all_cost[1] * 20);
+	item = "2";
+	if (!m2 && all_cost[1]>0){
+		m2= true
+		let minusButton = document.createElement("button");
+		minusButton.innerText = "del";
+		minusButton.classList.add("btn");
+		minusButton.classList.add("minus");
+		minusButton.addEventListener("click", function () {
+			if (all_cost[1] > 0) {
+				all_cost[1]--;
+				checkMainButton();
+				updateQuantity(1, all_cost[1]);
+				tg.MainButton.setText(all_cost[1] * 20);
+				if (all_cost[1]==0){
+					minusButton.remove();
+					m2=false
+				}
+			}
+			else{
+			}
+		});
+		let addButton = document.getElementById(`btn${item}`);
+
+		
+		addButton.insertAdjacentElement('afterend', minusButton);
+	}
+	tg.MainButton.show();
+});
+
+btn3.addEventListener("click", function () {
+	all_cost[2] = all_cost[2] + 1;
+	checkMainButton();
+	updateQuantity(2, all_cost[2]);
+	tg.MainButton.setText(all_cost[2] * 10+all_cost[2]*20+all_cost[2]*30);
+	item = "3";
+	if (!m3 && all_cost[2]>0){
+		m3= true
+		let minusButton = document.createElement("button");
+		minusButton.innerText = "del";
+		minusButton.classList.add("btn");
+		minusButton.classList.add("minus");
+		minusButton.addEventListener("click", function () {
+			if (all_cost[2] > 0) {
+				all_cost[2]--;
+				checkMainButton();
+				updateQuantity(2, all_cost[2]);
+				tg.MainButton.setText(all_cost[0] * 10+all_cost[1]*20+all_cost[2]*30);
+				if (all_cost[2]==0){
+					minusButton.remove();
+					m3=false
+				}
+			}
+			else{
+			}
+		});
+		let addButton = document.getElementById(`btn${item}`);
+
+		
+		addButton.insertAdjacentElement('afterend', minusButton);
+	}
+	tg.MainButton.show();
 });
 
 Telegram.WebApp.onEvent("mainButtonClicked", function () {
